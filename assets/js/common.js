36895e5d2ca720ec875786f4315f92fe895b0897
Fidel I. Schaposnik Massolo <fidel.s@gmail.com>
2022-06-29 18:26:27 +0200

    Don't ask why this is needed (??)

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     });
+    $('a.code').click(function() {
+        $(this).parent().parent().parent().find(".code.hidden").toggleClass('open');
+    });
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
